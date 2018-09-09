160814c791d836e04b79cbcbaba0349ed1891ce6
YashiBindal <42365838+YashiBindal@users.noreply.github.com>
2018-09-09 18:32:43 +0530

    Update FCFS.cpp

--- FCFS.cpp
+++ FCFS.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 int main()
 {   int n;
-    cin>>n;
+    cin>>n;                  //number of processes in schedular
 	int AT[n],BT[n]; 
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++)    //input AT and BT
     {
             cin>>AT[i];
           	cin>>BT[i];
